76b9edda990d39b42e95b88635a23381aa20b6ed
sumedhaKl <sumedhakamaraju@gmail.com>
2024-03-19 14:07:25 -0700

    edited titanic_model.py

--- api/titanic.py
+++ api/titanic.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
--- model/titanic_model.py
+++ model/titanic_model.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+
 # Load the titanic dataset
 titanic_data = sns.load_dataset('titanic')
 
