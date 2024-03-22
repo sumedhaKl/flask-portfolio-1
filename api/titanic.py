4d7002c4b6a690d3597749d05d69c8c0fce80d3e
jntpham <jordan.tri.pham@gmail.com>
2024-03-22 14:14:49 -0700

    api fix in progress

--- api/titanic.py
+++ api/titanic.py
@@ -4,10 +4,17 @@ from flask import Blueprint, request
 from flask_restful import Api, Resource
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
+from model.titanic_model import predict_model
 
 app = Flask(__name__)
 titanic_api = Blueprint('titanic_api', __name__, url_prefix='/api/titanic')
 api = Api(titanic_api)
+
+
+#Checks Titanic model
+model = predict_model()
+
+
 
 class Predict(Resource):
     def post(self):
@@ -15,20 +22,9 @@ class Predict(Resource):
             data = request.get_json()
             passenger_data = pd.DataFrame(data, index=[0])
             
-            # Preprocesssing
-            passenger_data['sex'] = passenger_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
-            passenger_data['alone'] = passenger_data['alone'].apply(lambda x: 1 if x == True else 0)
-            enc = OneHotEncoder(handle_unknown='ignore')
-            onehot = enc.transform(passenger_data[['embarked']]).toarray()
-            cols = ['embarked_' + val for val in enc.categories_[0]]
-            passenger_data[cols] = pd.DataFrame(onehot)
-            passenger_data.drop(['embarked'], axis=1, inplace=True)
+            #Returns from Titanic model api to frontend
+            return model.Predict()
             
-            # Predict the survival probability for the new passenger
-            logreg = LogisticRegression()
-            survival_prob = logreg.predict_proba(passenger_data)[:, 1]
-            death_prob = 1 - survival_prob
-
             return {'death_percentage': float(death_prob * 100), 'survivability_percentage': float(survival_prob * 100)}, 200
         except Exception as e:
             return {'error': str(e)}, 400
