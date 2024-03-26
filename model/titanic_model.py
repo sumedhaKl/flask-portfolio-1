950a8efc556899a98436a6d1470c3e53e5613036
sumedhaKl <sumedhakamaraju@gmail.com>
2024-03-26 13:51:27 -0700

    removed and modified code

--- model/titanic_model.py
+++ model/titanic_model.py
@@ -6,9 +6,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+import db
 
 class predict(db.Model):
-    
     def Predict():
         # Logistic regression model is used to predict the probability
         passenger = pd.DataFrame({
@@ -24,25 +24,39 @@ class predict(db.Model):
         })
 
         display(passenger)
-        new_passenger = passenger.copy()
+    new_passenger = passenger.copy()
 
         # Preprocess the new passenger data
-        new_passenger['sex'] = new_passenger['sex'].apply(lambda x: 1 if x == 'male' else 0)
-        new_passenger['alone'] = new_passenger['alone'].apply(lambda x: 1 if x == True else 0)
+    new_passenger['sex'] = new_passenger['sex'].apply(lambda x: 1 if x == 'male' else 0)
+    new_passenger['alone'] = new_passenger['alone'].apply(lambda x: 1 if x == True else 0)
 
         # Encode 'embarked' variable
-        onehot = enc.transform(new_passenger[['embarked']]).toarray()
-        cols = ['embarked_' + val for val in enc.categories_[0]]
-        new_passenger[cols] = pd.DataFrame(onehot, index=new_passenger.index)
-        new_passenger.drop(['name'], axis=1, inplace=True)
-        new_passenger.drop(['embarked'], axis=1, inplace=True)
+    onehot = enc.transform(new_passenger[['embarked']]).toarray()
+    cols = ['embarked_' + val for val in enc.categories_[0]]
+    new_passenger[cols] = pd.DataFrame(onehot, index=new_passenger.index)
+    new_passenger.drop(['name'], axis=1, inplace=True)
+    new_passenger.drop(['embarked'], axis=1, inplace=True)
 
-        display(new_passenger)
+    display(new_passenger)
 
-        # Predict the survival probability for the new passenger
-        dead_proba, alive_proba = np.squeeze(logreg.predict_proba(new_passenger))
+   # Split arrays in random train 70%, random test 30%, using stratified sampling (same proportion of survived in both sets) and a fixed random state (42
+    # The number 42 is often used in examples and tutorials because of its cultural significance in fields like science fiction (it's the "Answer to the Ultimate Question of Life, The Universe, and Everything" in The Hitchhiker's Guide to the Galaxy by Douglas Adams). But in practice, the actual value doesn't matter; what's important is that it's set to a consistent value.
+    # X_train is the DataFrame containing the features for the training set.
+    # X_test is the DataFrame containing the features for the test set.
+    # y-train is the 'survived' status for each passenger in the training set, corresponding to the X_train data.
+    # y_test is the 'survived' status for each passenger in the test set, corresponding to the X_test data.
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+    # Train a logistic regression model
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
 
+    # Test the model
+    y_pred = logreg.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Predict the survival probability for the new passenger
+    dead_proba, alive_proba = np.squeeze(logreg.predict_proba(new_passenger))
   
 def initTitanicData():
       # Load the titanic dataset
@@ -66,21 +80,3 @@ def initTitanicData():
     # Build distinct data frames on survived column
     X = td.drop('survived', axis=1) # all except 'survived'
     y = td['survived'] # only 'survived'
-
-    # Split arrays in random train 70%, random test 30%, using stratified sampling (same proportion of survived in both sets) and a fixed random state (42
-    # The number 42 is often used in examples and tutorials because of its cultural significance in fields like science fiction (it's the "Answer to the Ultimate Question of Life, The Universe, and Everything" in The Hitchhiker's Guide to the Galaxy by Douglas Adams). But in practice, the actual value doesn't matter; what's important is that it's set to a consistent value.
-    # X_train is the DataFrame containing the features for the training set.
-    # X_test is the DataFrame containing the features for the test set.
-    # y-train is the 'survived' status for each passenger in the training set, corresponding to the X_train data.
-    # y_test is the 'survived' status for each passenger in the test set, corresponding to the X_test data.
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-    # Train a logistic regression model
-    logreg = LogisticRegression()
-    logreg.fit(X_train, y_train)
-
-    # Test the model
-    y_pred = logreg.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    
-   
