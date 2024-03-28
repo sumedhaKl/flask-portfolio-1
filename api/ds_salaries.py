0fbcfe56fb8fa3dbca1af78b05985198d9dca838
sumedhaKl <sumedhakamaraju@gmail.com>
2024-03-28 13:10:49 -0700

    modified ds_salaries.py

--- api/ds_salaries.py
+++ api/ds_salaries.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import pandas as pd
-from flask import Blueprint, request
+from flask import Blueprint
 from flask_restful import Api, Resource
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
@@ -15,17 +15,17 @@ class Predict(Resource):
             salary_data = pd.read_csv("ds_salaries.csv")
             
             # Preprocesssing
-            salary_data['sex'] = passenger_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
-            salary_data['alone'] = passenger_data['alone'].apply(lambda x: 1 if x == True else 0)
+            salary_data['sex'] = salary_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
+            salary_data['alone'] = salary_data['alone'].apply(lambda x: 1 if x == True else 0)
             enc = OneHotEncoder(handle_unknown='ignore')
-            onehot = enc.transform(passenger_data[['embarked']]).toarray()
+            onehot = enc.transform(salary_data[['embarked']]).toarray()
             cols = ['embarked_' + val for val in enc.categories_[0]]
-            passenger_data[cols] = pd.DataFrame(onehot)
-            passenger_data.drop(['embarked'], axis=1, inplace=True)
+            salary_data[cols] = pd.DataFrame(onehot)
+            salary_data.drop(['embarked'], axis=1, inplace=True)
             
             # Predict the survival probability for the new passenger
             logreg = LogisticRegression()
-            survival_prob = logreg.predict_proba(passenger_data)[:, 1]
+            survival_prob = logreg.predict_proba(salary_data)[:, 1]
             death_prob = 1 - survival_prob
 
             return {'death_percentage': float(death_prob * 100), 'survivability_percentage': float(survival_prob * 100)}, 200
