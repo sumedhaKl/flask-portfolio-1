077a80f6b8655ade821583f6b1f4e7f141d1650b
sumedhaKl <sumedhakamaraju@gmail.com>
2024-03-31 15:58:46 -0700

    modifed code

--- api/ds_salaries.py
+++ api/ds_salaries.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import pandas as pd
 from flask import Blueprint
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 
@@ -12,22 +12,32 @@ api = Api(salaries_api)
 class Predict(Resource):
     def post(self):
         try:
+            # Parse incoming request data
+            parser = reqparse.RequestParser()
+            parser.add_argument('job_title', type=str, required=True)
+            parser.add_argument('experience_level', type=str, required=True)
+            args = parser.parse_args()
+            
+            # Read CSV data
             salary_data = pd.read_csv("ds_salaries.csv")
             
-            # Preprocesssing
+            # Preprocessing
+            salary_data['job_title'] = args['job_title']
+            salary_data['experience_level'] = args['experience_level']
             salary_data['job_title'] = salary_data['job_title'].apply(lambda x: 1 if x else 0)
             salary_data['experience_level'] = salary_data['experience_level'].apply(lambda x: 1 if x else 0)
             enc = OneHotEncoder(handle_unknown='ignore')
-            onehot = enc.transform(salary_data[['salary']]).toarray()
-            cols = ['salary_' + val for val in enc.categories_[0]]
+            onehot = enc.transform(salary_data[['job_title', 'experience_level']]).toarray()
+            cols = ['job_title_' + val for val in enc.categories_[0]]
+            cols += ['experience_level_' + val for val in enc.categories_[1]]
             salary_data[cols] = pd.DataFrame(onehot)
-            salary_data.drop(['salary'], axis=1, inplace=True)
+            salary_data.drop(['job_title', 'experience_level'], axis=1, inplace=True)
             
-            # Predict the survival probability for the new passenger
+            # Predict the salary probability for the new data
             logreg = LogisticRegression()
             salary_prob = logreg.predict_proba(salary_data)[:, 1]
 
-            return {'salary': float(salary_prob)}
+            return {'salary_probability': float(salary_prob)}
         except Exception as e:
             return {'error': str(e)}, 400
 
