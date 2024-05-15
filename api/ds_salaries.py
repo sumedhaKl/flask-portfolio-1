a2be2cfb0d3abfd0ed0d090f0736be4afdaeb0b0
sumedhaKl <sumedhakamaraju@gmail.com>
2024-05-14 20:03:44 -0700

    first commit

--- api/ds_salaries.py
+++ api/ds_salaries.py
@@ -1,145 +1,108 @@
-from flask import Blueprint, request, jsonify
+from flask import Flask, Blueprint, request, jsonify
 from flask_restful import Api, Resource, reqparse
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import LogisticRegression
+from flask_cors import CORS
 
-salaries_api = Blueprint('salaries_api', __name__, url_prefix='/api/salaries')
+app = Flask(__name__)
+salaries_api = Blueprint('api', __name__, url_prefix='/api')
+CORS(salaries_api)
 api = Api(salaries_api)
 
-class SalaryModel:
-    _instance = None
-
-    def __init__(self):
-        self.model = None
-        self.encoder = OneHotEncoder(handle_unknown='ignore')
-
-    def _clean(self):
-        csv_file_path  = ('/home/sumi/vscode/flask-portfolio-1/ds_salaries.csv')
-        self.salary_data = pd.read_csv(csv_file_path)
-        self.salary_data['work_year'] = pd.to_numeric(self.salary_data['work_year'])
-        self.salary_data['salary'] = pd.to_numeric(self.salary_data['salary'])
-        self.salary_data['salary_in_usd'] = pd.to_numeric(self.salary_data['salary_in_usd'])
-        self.salary_data['remote_ratio'] = pd.to_numeric(self.salary_data['remote_ratio'])
-
-        self.salary_data.dropna(inplace=True)
-        
-        self.features = ['experience_level', 'employment_type', 'job_title', 'salary_currency', 'salary_in_usd', 'employee_residence', 'remote_ratio', 'company_location', 'company_size']
-        
-        self.encoder.fit(self.salary_data[self.features])
-        
-    def _train(self):
-        # Train the model
-        X = self.salary_data[['work_year'] + self.features[1:]]
-        y = self.salary_data['salary']
-        self.model = LogisticRegression()
-        self.model.fit(X, y)
-
-    def predict(self, data):
+class SalaryPrediction(Resource):
+    def post(self):
         try:
-            work_year = float(data['work_year'])
-            salary_in_usd = float(data['salary_in_usd'])
-            remote_ratio = float(data['remote_ratio'])
-        
+            parser = reqparse.RequestParser()
+            parser.add_argument('work_year', type=int, required=True)
+            parser.add_argument('experience_level', type=str, required=True)
+            parser.add_argument('employment_type', type=str, required=True)
+            parser.add_argument('job_title', type=str, required=True)
+            parser.add_argument('salary_currency', type=str, required=True)
+            parser.add_argument('salary_in_usd', type=float, required=True)
+            parser.add_argument('employee_residence', type=str, required=True)
+            parser.add_argument('remote_ratio', type=float, required=True)
+            parser.add_argument('company_location', type=str, required=True)
+            parser.add_argument('company_size', type=str, required=True)
+            args = parser.parse_args()
+
+            print("Received data:", args)
+
+            received_experience_level = args['experience_level']
+
+            print("Received experience level:", received_experience_level)
+
             experience_level_mapping = {
                 'entry': 'EN',
                 'mid': 'MI',
                 'senior': 'SE',
                 'expert': 'EX'     
             }
-            experience_level = experience_level_mapping.get[data['experience_level'].lower()]
-            if experience_level is None:
+
+            mapped_experience_level = experience_level_mapping.get(received_experience_level.lower())
+
+            print("Mapped experience level:", mapped_experience_level)
+
+            if mapped_experience_level is None:
                 raise ValueError("Invalid experience level")
+            else:
+                print("Experience level is valid:", mapped_experience_level)
 
             job_title_mapping = {
-            'Data Scientist': 1,
-            'Data Analyst': 2,
-            'Data Engineer': 3,
-            'Machine Learning Scientist': 4,
-            'Big Data Engineer': 5,
-            'Product Data Analyst': 6,
-            'Machine Learning Engineer': 7,
-            'Lead Data Scientist': 8,
-            'Business Data Analyst': 9,
-            'Lead Data Engineer': 10,
-            'Lead Data Analyst': 11,
-            'Data Scientist Consultant': 12,
-            'BI Data Analyst': 13,
-            'Director of Data Science': 14,
-            'Research Scientist': 15,
-            'Machine Learning Manager': 16,
-            'Data Engineering Manager': 17,
-            'Machine Learning Infrastructure Engineer': 18,
-            'ML Engineer': 19,
-            'AI Scientist': 20,
-            'Computer Vision Engineer': 21,
-            'Principal Data Scientist': 22,
-            'Head of Data': 23,
-            '3D Computer Vision Researcher': 24,
-            'Applied Data Scientist': 25,
-            'Marketing Data Analyst': 26,
-            'cloud data engineer': 27,
-            'financial data analyst': 28,
-            'computer vision software engineer': 29,
-            'data science manager': 30,
-            'data analytics engineer': 31,
-            'applied machine learning scientist': 32,
-            'data specialist': 33,
-            'data science engineer': 34,
-            'big data architect': 35,
-            'head of data science': 36,
-            'analytics engineer': 37,
-            'data architect': 38,
-            'head of machine learning': 39,
-            'etl developer': 40,
-            'lead machine learning engineer': 41,
-            'machine learning developer': 42,
-            'principal data analyst': 43,
-            'nlp engineer': 44,
-            'data analytics lead': 45
-        }
-            job_title = job_title_mapping.get[data['job_title'].lower()]
+                'Data Scientist': 1,
+                'Data Analyst': 2,
+                'Data Engineer': 3,
+                'Machine Learning Scientist': 4,
+                'Big Data Engineer': 5,
+                'Product Data Analyst': 6,
+                'Machine Learning Engineer': 7,
+                'Lead Data Scientist': 8,
+                'Business Data Analyst': 9,
+                'Lead Data Engineer': 10,
+                'Lead Data Analyst': 11,
+                'Data Scientist Consultant': 12,
+                'BI Data Analyst': 13,
+                'Director of Data Science': 14,
+                'Research Scientist': 15,
+                'Machine Learning Manager': 16,
+                'Data Engineering Manager': 17,
+                'Machine Learning Infrastructure Engineer': 18,
+                'ML Engineer': 19,
+                'AI Scientist': 20,
+                'Computer Vision Engineer': 21,
+                'Principal Data Scientist': 22,
+                'Head of Data': 23,
+                '3D Computer Vision Researcher': 24,
+                'Applied Data Scientist': 25,
+                'Marketing Data Analyst': 26,
+                'cloud data engineer': 27,
+                'financial data analyst': 28,
+                'computer vision software engineer': 29,
+                'data science manager': 30,
+                'data analytics engineer': 31,
+                'applied machine learning scientist': 32,
+                'data specialist': 33,
+                'data science engineer': 34,
+                'big data architect': 35,
+                'head of data science': 36,
+                'analytics engineer': 37,
+                'data architect': 38,
+                'head of machine learning': 39,
+                'etl developer': 40,
+                'lead machine learning engineer': 41,
+                'machine learning developer': 42,
+                'principal data analyst': 43,
+                'nlp engineer': 44,
+                'data analytics lead': 45
+            }
+            job_title = job_title_mapping.get(args['job_title'], None)
             if job_title is None:
                 raise ValueError("Invalid job title")
-        
-            input_data = pd.DataFrame([[work_year, experience_level, job_title, salary_in_usd, remote_ratio]], columns=['work_year'] + self.features[1:])
-        
-            input_data[self.features] = self.encoder.transform(input_data[self.features])
-        
-            salary_probability = self.model.predict_proba(input_data)[:, 1]
-            return float(salary_probability)
-        except (ValueError, KeyError) as e:
-            return {'error': str(e)}, 400
-    
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-            cls._instance._clean()
-            cls._instance._train()
-        return cls._instance
 
-class Predict(Resource):
-    def post(self):
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('work_year', type=str, required=True)
-            parser.add_argument('experience_level', type=str, required=True)
-            parser.add_argument('employment_type', type=str, required=True)               
-            parser.add_argument('job_title', type=str, required=True)
-            parser.add_argument('salary_currency', type=str, required=True)
-            parser.add_argument('salary_in_usd', type=str, required=True)
-            parser.add_argument('employee_residence', type=str, required=True)
-            parser.add_argument('remote_ratio', type=str, required=True)
-            parser.add_argument('company_location', type=str, required=True)
-            parser.add_argument('company_size', type=str, required=True)
-            args = parser.parse_args()
-
-            salary_model = SalaryModel.get_instance()
-            salary_prob = salary_model.predict(args)
-
-            return {'salary_probability': salary_prob}
+            return {'success': True}, 200
+        
         except Exception as e:
             return {'error': str(e)}, 400
 
-api.add_resource(Predict, '/predict')
+api.add_resource(SalaryPrediction, '/salaries/predict')
+
+if __name__ == '__main__':
+    app.register_blueprint(salaries_api)
+    app.run(debug=True)
--- model/salaries_model.py
+++ model/salaries_model.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask
+from flask import Flask, Blueprint
 from flask_restful import Api, Resource, reqparse
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import OneHotEncoder
@@ -17,7 +17,7 @@ class SalaryModel(Resource):
     
     def __init__(self):
         self.model = None
-        self.features = ['work_year', 'salary_in_usd', 'remote_ratio']
+        self.features = ['work_year', 'experience_level', 'employment_type', 'salary_in_usd', 'remote_ratio']
         self.target = 'salary'
         
         encoder = OneHotEncoder(handle_unknown='ignore')
@@ -36,20 +36,32 @@ class SalaryModel(Resource):
         X = self.salary_data[self.features]
         y = self.salary_data[self.target]
         
-        self.model.fit(X,y)
+        self.model.fit(X, y)
     
+    def predict_salary(self, input_data):
+        try:
+            input_features = pd.DataFrame([input_data], columns=self.features)
+            input_features_encoded = self.encoder.transform(input_features[['experience_level', 'employment_type']]).toarray()
+            input_features.drop(['experience_level', 'employment_type'], axis=1, inplace=True)
+            input_features = pd.concat([input_features, pd.DataFrame(input_features_encoded)], axis=1)
+
+            probability = self.model.predict_proba(input_features)[0, 1]
+            return {'probability': probability}, 200
+        except Exception as e:
+            return {'error': str(e)}, 400
+
     def post(self):
         try:
             parser = reqparse.RequestParser()
             parser.add_argument('work_year', type=float, required=True)
+            parser.add_argument('experience_level', type=str, required=True)
+            parser.add_argument('employment_type', type=str, required=True)
             parser.add_argument('salary_in_usd', type=float, required=True)
             parser.add_argument('remote_ratio', type=float, required=True)
             args = parser.parse_args()
-            
-            input_features = pd.DataFrame([args], columns=self.features)
-            
-            probability = self.model.predict_proba(input_features)[0, 1] 
-            return {'probability': probability}, 200
+
+            prediction, status_code = self.predict_salary(args)
+            return prediction, status_code
         except Exception as e:
             return {'error': str(e)}, 400
 
