535a8a7c1d3df908b6f8ce29cbeb7950c7af8238
sumedhaKl <sumedhakamaraju@gmail.com>
2024-05-20 16:04:41 +0000

    modified code

--- model/salaries_model.py
+++ model/salaries_model.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
 from flask import Blueprint, Flask
 from flask_restful import Api, reqparse
->>>>>>> 60edfde1c777b01b790d6b3b08932580343754da
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
-<<<<<<< HEAD
-class SalaryModel:
-=======
 app = Flask(__name__)
 salaries_api = Blueprint('salaries_api', __name__, url_prefix='/api/salaries')
 api = Api(salaries_api)
@@ -19,26 +13,16 @@ df = pd.read_csv(csv_file_path)
 
 class SalaryModel:
     
->>>>>>> 60edfde1c777b01b790d6b3b08932580343754da
     _instance = None
 
     def __init__(self):
         self.model = None
-<<<<<<< HEAD
-        self.encoder = OneHotEncoder(handle_unknown='ignore')
-        self.features = ['work_year', 'experience_level', 'employment_type', 'salary_in_usd', 'remote_ratio']
-        self.target = 'salary'
-
-        csv_file_path = "/home/kasm-user/vscode/flask-portfolio-1/ds_salaries.csv"
-        self.salary_data = pd.read_csv(csv_file_path)
-=======
         self.features = ['work_year','salary_in_usd', 'remote_ratio']
         self.target = 'salary'
         
         encoder = OneHotEncoder(handle_unknown='ignore')
         self.salary_data = df
         self.encoder = encoder 
->>>>>>> 60edfde1c777b01b790d6b3b08932580343754da
         
         self._clean()
         self._train()
@@ -51,26 +35,6 @@ class SalaryModel:
         y = self.salary_data[self.target]
         self.model = LogisticRegression(max_iter=1000)
         self.model.fit(X, y)
-<<<<<<< HEAD
-
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-        return cls._instance
-
-    def predict_salary(self, input_data):
-        try:
-            input_features = pd.DataFrame([input_data], columns=self.features)
-            input_features_encoded = self.encoder.transform(input_features[['experience_level', 'employment_type']]).toarray()
-            input_features.drop(['experience_level', 'employment_type'], axis=1, inplace=True)
-            input_features = pd.concat([input_features, pd.DataFrame(input_features_encoded)], axis=1)
-
-            probability = self.model.predict_proba(input_features)[0, 1]
-            return {'probability': probability}, 200
-        except Exception as e:
-            return {'error': str(e)}, 400
-=======
         
     def predict_salary(self, input_data):
         try:
@@ -91,4 +55,3 @@ api.add_resource(SalaryModel, '/predict')
 if __name__ == '__main__':
     app.register_blueprint(salaries_api)
     app.run(debug=True)
->>>>>>> 60edfde1c777b01b790d6b3b08932580343754da
